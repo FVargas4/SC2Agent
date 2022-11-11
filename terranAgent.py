93e28a51c22ef903148c63d4c9c43df6f8c4e391
FVargas4 <fvargascode@gmail.com>
2022-11-10 21:42:59 -0600

    Final Agent Hand-in 1

--- terranAgent.py
+++ terranAgent.py
@@ -4,82 +4,172 @@ from pysc2.lib import actions, features, units
 from absl import app
 import random
 
+
 #obs is the object that contains all the observactions we need it
 
+"""
+
+  Hand-in I: StarCraft II Terran Agent
+    Author: Fernando Vargas
+
+    :)
+  ________________________________________________________________________
+
+  TerranAgent (class): Agent object that contains all needed methods to win 
+  vs an easy level Agent.
+
+  main (class): Main code that creates and activates both Terran and an easy
+  level (Random Race) Agent.
+  
+"""
 class TerranAgent(base_agent.BaseAgent):
 
-  # Constructor
+  # Constructor function
   def __init__(self):
-    super(TerranAgent, self).__init__()  
+    # Initiate Agent 
+    super(TerranAgent, self).__init__()
+    # TerranAgent.atack_coordinates (array): Coordinates to where the attack heading.  
     self.attack_coordinates = None
   
-  # Checar si el tipo de unidad es seleccionada
+  """
+  
+  Function unit_type_is_selected(self, observation object, Tag) [UTIS]
+  
+    self (class): Allows the agent to access itself.
+    obs (object): Object that acts as the sensors of the agent.
+    unit_type (Tag): API's internal Tag that identifies the type of unit. 
+  
+  """
   def unit_type_is_selected(self, obs, unit_type):
   
-    """utility fuction to simply sintax of unit type 
-    selected check"""
+    # Single selection conditional
     if (len(obs.observation.single_select) > 0 and obs.observation.single_select[0].unit_type == unit_type):
         return True
-        
+    # Multi selection conditional
     if (len(obs.observation.multi_select) > 0 and obs.observation.multi_select[0].unit_type == unit_type):
         return True
         
     return False
 
+  """
+  
+  Function get_units_by_type(self, observation object, Tag)
+
+    Function that get units by type
+  
+    self (class): Allows the agent to access itself.
+    obs (object): Object that acts as the sensors of the agent.
+    unit_type (Tag): API's internal Tag that identifies the type of unit. 
+  
+  """
   def get_units_by_type(self, obs, unit_type):
     return [unit for unit in obs.observation.feature_units
             if unit.unit_type == unit_type]
-    
+  """
+  
+  Function can_do(self, observation object, Tag)
+
+    Function that determines whether the agent can do an action or not
+  
+    self (class): Allows the agent to access itself.
+    obs (object): Object that acts as the sensors of the agent.
+    action (Tag): API's internal Tag that identifies the action desired. 
+  
+  """
   def can_do(self, obs, action):
     return action in obs.observation.available_actions
 
+  """
+  H-i I
+  Function build_refinery(self, observation object)
+  
+    self (class): Allows the agent to access itself.
+    obs (object): Object that acts as the sensors of the agent.
+  _________________________________________________________________________________________
+
+  vespene_geysers (array): Array that contain the vespene geysers in the screen.
+  refineries (array): Array that contain the refineries in the screen.
+
+  """
   def build_refinery(self, obs):
+
     vespene_geysers = self.get_units_by_type(obs, units.Neutral.VespeneGeyser)
     refineries = self.get_units_by_type(obs, units.Terran.Refinery)
-
+    
+    # If there is at least a refinery and there are any vespene geysers
     if len(refineries) < 1 and len(vespene_geysers) > 0:
+
+        # If the SCV is selected by the agent
         if self.unit_type_is_selected(obs, units.Terran.SCV):
+
+          # If the agent can build a Refinery
           if self.can_do(obs, actions.FUNCTIONS.Build_Refinery_screen.id):
+            # Choose a random geyser
             geyser = random.choice(vespene_geysers)
+            # Build a refinery at the chosen geyser 
             return actions.FUNCTIONS.Build_Refinery_screen("now", (geyser.x, geyser.y))
-
+        
+        # Find the SCVs and store it in variable scvs
         scvs = self.get_units_by_type(obs, units.Terran.SCV)
 
+        # If the scvs array is not empty
         if len(scvs) > 0:
+
+          # Choose a random SCV
           scv = random.choice(scvs)
+          # Tell the agent to select all SCVs
           return actions.FUNCTIONS.select_point("select_all_type", (scv.x, scv.y))
-
-  def gather_vespene_gas(self,obs):
-    refinery = self.get_units_by_type(obs, units.Terran.Refinery)
-    if len(refinery) > 0:
-      refinery = random.choice(refinery)
-      if refinery['assigned_harvesters'] < 3:
-        if self.unit_type_is_selected(obs, units.Terran.SCV):
-          if len(obs.observation.single_select) < 2 and len(obs.observation.multi_select) < 2:
-            if self.can_do(obs,actions.FUNCTIONS.Harvest_Gather_screen.id):
-
-              return actions.FUNCTIONS.Harvest_Gather_screen("now",(refinery.x, refinery.y))
-                            
-        scvs = self.get_units_by_type(obs, units.Terran.SCV)
-        if len(scvs) > 0 :
-          scv = random.choice(scvs)
-          return actions.FUNCTIONS.select_point("select",(scv.x,scv.y))
+  """
+  H-i I
+  Function gather_vespene_gas(self, observation object)
   
-  def upgrade_terrans(self, obs):
-    enbase = self.get_units_by_type(obs, units.Terran.EngineeringBay)
-    materials = obs.observation.player.minerals
-    vespene = obs.observation.player.vespene_gas
+    self (class): Allows the agent to access itself.
+    obs (object): Object that acts as the sensors of the agent.
+  ________________________________________________________________________
 
-    if len(enbase) < 1 and len(materials) > 100 and len(vespene) > 100:
-        if self.unit_type_is_selected(obs, units.Terran.EngineeringBay):
-          bay = random.choice(enbase)
-          if self.can_do(obs, actions.FUNCTIONS.Research_TerranInfantryArmorLevel1_quick.id):
-            x = random.randint(0, 83)
-            y = random.randint(0, 83)
-            return actions.FUNCTIONS.Research_TerranInfantryArmorLevel1_quick('now', ( x, y))
+    refineries (array): Array that contain the fineries in the screen.
+  
+  """
+  def gather_vespene_gas(self,obs):
+    
+    # Find Refineries in the screen and save it as an array in variable refineries
+    refineries = self.get_units_by_type(obs, units.Terran.Refinery)
 
+    # If refineries is not empty
+    if len(refineries) > 0:
+      # Choose a refinery where the vespene gas will be recollected and store it in variable refinery
+      refinery = random.choice(refineries)
+      # If there are at least 3 harvesters
+      if refinery['assigned_harvesters'] < 3:
+        # If the SCV is selected 
+        if self.unit_type_is_selected(obs, units.Terran.SCV):
+          # If the observation objects selects (multi or single) 
+          if len(obs.observation.single_select) < 2 and len(obs.observation.multi_select) < 2:
+            # If the agent can do "harvest_gather" function
+            if self.can_do(obs,actions.FUNCTIONS.Harvest_Gather_screen.id):
+              # Use the actuators library actions to make the agent gather gas
+              return actions.FUNCTIONS.Harvest_Gather_screen("now",(refinery.x, refinery.y))
 
+        # Find the SCVs and store it in variable scvs         
+        scvs = self.get_units_by_type(obs, units.Terran.SCV)
+
+        # If the scvs array is not empty
+        if len(scvs) > 0 :
+          # Choose a random SCV
+          scv = random.choice(scvs)
+          # Tell the agent to select all SCVs
+          return actions.FUNCTIONS.select_point("select",(scv.x,scv.y))
+
+  """
+  
+  Function step(self, observation object)
+  
+    self (class): Allows the agent to access itself.
+    obs (object): Object that acts as the sensors of the agent.
+  
+  """
   def step(self, obs):
+
     super(TerranAgent, self).step(obs)
     
     #select/guess the location of the enemies
@@ -96,80 +186,125 @@ class TerranAgent(base_agent.BaseAgent):
             #set pair of coordintates
             self.attack_coordinates = [12,16]
 
-    # Create attack conditions
-    marines = self.get_units_by_type(obs, units.Terran.Marine)
-    if len(marines) >= 10:
-      if self.unit_type_is_selected(obs, units.Terran.Marine):
-        if self.can_do(obs, actions.FUNCTIONS.Attack_minimap.id):
-          return actions.FUNCTIONS.Attack_minimap('now', self.attack_coordinates)  
-      if self.can_do(obs, actions.FUNCTIONS.select_army.id):
-        return actions.FUNCTIONS.select_army('select')
-
     # Obtain quantity of minerals
     minerals = obs.observation.player.minerals
 
     # Get Supply Depot as terrenian. Minerals available needed.                          
     terranian = self.get_units_by_type(obs, units.Terran.SupplyDepot)
-    if len(terranian) < 2 and minerals >= 100:
-      if self.unit_type_is_selected(obs, units.Terran.SCV):
-        if self.can_do(obs, actions.FUNCTIONS.Build_SupplyDepot_screen.id):
-          x = random.randint(0, 83)
-          y = random.randint(0, 83)
-          return actions.FUNCTIONS.Build_SupplyDepot_screen('now', ( x, y))
- 
-                                
+
+    # Check if there are marine, if there is no marine build one. Minerals available needed.
+    marines = self.get_units_by_type(obs, units.Terran.Marine)
+
     # Check if there are barracks, if there is no barraks build one. Minerals available needed.
     barracks = self.get_units_by_type(obs, units.Terran.Barracks)
-    if len (barracks) < 3 and minerals >= 150:
-        if self.unit_type_is_selected(obs, units.Terran.SCV):
-          if self.can_do(obs, actions.FUNCTIONS.Build_Barracks_screen.id):
-            x = random.randint(0, 83)
-            y = random.randint(0, 83)
-            return actions.FUNCTIONS.Build_Barracks_screen('now', ( x, y))
-
-# Check if there are barracks, if there is no barraks build one. Minerals available needed.
+    
+    # Check if there are barracks, if there is no barraks build one. Minerals available needed.
     enbase = self.get_units_by_type(obs, units.Terran.EngineeringBay)
-    if len (enbase) < 2 and minerals >= 125:
-        if self.unit_type_is_selected(obs, units.Terran.SCV):
-          if self.can_do(obs, actions.FUNCTIONS.Build_EngineeringBay_screen.id):
-            x = random.randint(0, 83)
-            y = random.randint(0, 83)
-            return actions.FUNCTIONS.Build_EngineeringBay_screen('now', ( x, y))
 
+    # Select SCV units 
+    scvs = self.get_units_by_type(obs, units.Terran.SCV)
 
-    if len(barracks) >= 3:
-      if self.unit_type_is_selected(obs, units.Terran.Barracks):
-        marines = self.get_units_by_type(obs, units.Terran.Marine)
-        if len(marines) <= 10:
-          if self.can_do(obs, actions.FUNCTIONS.Train_Marine_quick.id):
-            return actions.FUNCTIONS.Train_Marine_quick('now')
-      b = random.choice(barracks)
-      return actions.FUNCTIONS.select_point('select_all_type', (b.x, b.y))
-        
-        
+    # Call GVG function to gather Vespene Gas
+    g_refinery = self.gather_vespene_gas(obs)
+
+    # Call BR function to build Refinary
     b_refinery = self.build_refinery(obs)
+
+    """Create attack conditions"""
+
+    # If there are at least 10 marines
+    if len(marines) >= 12:
+      # Select Marines
+      if self.unit_type_is_selected(obs, units.Terran.Marine):
+        # If you can check enemies' location on the minimap
+        if self.can_do(obs, actions.FUNCTIONS.Attack_minimap.id):
+          # Send marines to attack them
+          return actions.FUNCTIONS.Attack_minimap('now', self.attack_coordinates)  
+      # If the selected unit is not Marine
+      if self.can_do(obs, actions.FUNCTIONS.select_army.id):
+        # Select the army (marines)
+        return actions.FUNCTIONS.select_army('select')
+    
+    """Supply Depot (spawner) conditions"""
+
+    # If there are not at least 2 supply depots and the agent has 100 materials
+    if len(terranian) < 2 and minerals >= 100:
+      # If the unit selected is a SCV
+      if self.unit_type_is_selected(obs, units.Terran.SCV):
+        # If it is possible to build a Supply Depot
+        if self.can_do(obs, actions.FUNCTIONS.Build_SupplyDepot_screen.id):
+          # Get [x, y] values randomly from 0 to 83 in both cases
+          x = random.randint(0, 83)
+          y = random.randint(0, 83)
+          # Build the supply depot on the random coordinates
+          return actions.FUNCTIONS.Build_SupplyDepot_screen('now', ( x, y))
+      
+    # If there are not at least 3 barracks and the agent has 150 materials
+    if len (barracks) < 3 and minerals >= 150:
+      # If the unit selected is a SCV
+      if self.unit_type_is_selected(obs, units.Terran.SCV):
+        # If it is possible to build a Barrak
+        if self.can_do(obs, actions.FUNCTIONS.Build_Barracks_screen.id):
+          # Get [x, y] values randomly from 0 to 83 in both cases
+          x = random.randint(0, 83)
+          y = random.randint(0, 83)
+          # Build the supply depot on the barracks coordinates
+          return actions.FUNCTIONS.Build_Barracks_screen('now', ( x, y))
+
+    # If there are not at least 2 engineering bay and the agent has 125 materials
+    if len (enbase) < 2 and minerals >= 125:
+      # If the unit selected is a SCV
+      if self.unit_type_is_selected(obs, units.Terran.SCV):
+        # If it is possible to build a Engineering Bay
+        if self.can_do(obs, actions.FUNCTIONS.Build_EngineeringBay_screen.id):
+          # Get [x, y] values randomly from 0 to 60 in both cases
+          x = random.randint(0, 60)
+          y = random.randint(0, 60)
+          # Build the Engineering Bay on the random coordinates
+          return actions.FUNCTIONS.Build_EngineeringBay_screen('now', ( x, y))
+
+    # If there are not at least 2 engineering bay and the agent has 125 materials
+    if len(barracks) >= 3:
+      # If the unit selected is a Barrak
+      if self.unit_type_is_selected(obs, units.Terran.Barracks):
+        # If there are less than 10 marines
+        if len(marines) <= 10:
+          # If it is possible to build a train (create) mairnes
+          if self.can_do(obs, actions.FUNCTIONS.Train_Marine_quick.id):
+            # Spawn Marines
+            return actions.FUNCTIONS.Train_Marine_quick('now')
+      # Choose a random barrack
+      b = random.choice(barracks)
+      # Select point at barracks coordinates
+      return actions.FUNCTIONS.select_point('select_all_type', (b.x, b.y))
+
+    # if b_refinery runs correctly, return it  
     if b_refinery:
         return b_refinery
-
-    #Select SCV units 
-    scvs = self.get_units_by_type(obs, units.Terran.SCV)
-    if len(scvs) > 0:
-        scv = random.choice(scvs)
-        return actions.FUNCTIONS.select_point("select_all_type", (scv.x, scv.y))
-
-    # Gather Vespene Gas
-    g_refinery = self.gather_vespene_gas(obs)
+    
+    # if g_refinery runs correctly, return it  
     if g_refinery:
           return g_refinery
 
-    # upgade_weapons = self.upgrade_terrans(obs)
-    # if upgade_weapons:
-    #       return upgade_weapons
+    # If the scvs array is not empty
+    if len(scvs) > 0:
+        # Choose a random SCV
+        scv = random.choice(scvs)
+        # Tell the agent to select all SCVs
+        return actions.FUNCTIONS.select_point("select_all_type", (scv.x, scv.y))
 
-    
-    
+    # If there is nothing left to do, run the no_op function
     return actions.FUNCTIONS.no_op()
 
+  """
+  
+  Function unit_type_is_selected(self, observation object, Tag)
+  
+    self (class): Allows the agent to access itself.
+    obs (object): Object that acts as the sensors of the agent.
+    unit_type (Tag): API's internal Tag that identifies the type of unit. 
+  
+  """
 def main(unused_argv):
   agent = TerranAgent()
   try:
@@ -177,7 +312,7 @@ def main(unused_argv):
       with sc2_env.SC2Env(
           map_name="Simple64",
           players=[sc2_env.Agent(sc2_env.Race.terran),
-                   sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.easy)],
+                   sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.very_easy)],
           agent_interface_format=features.AgentInterfaceFormat(
               feature_dimensions=features.Dimensions(screen=84, minimap=64),
               use_feature_units=True),
@@ -197,6 +332,9 @@ def main(unused_argv):
           timesteps = env.step(step_actions)
       
   except KeyboardInterrupt:
+    pass
+  except ValueError:
+    print('Crash')
     pass
   
 if __name__ == "__main__":
