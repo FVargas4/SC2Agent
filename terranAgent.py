327d971b3b4c21d6339f4baac6ffda2554f12ab8
FVargas4 <fvargascode@gmail.com>
2022-11-14 21:20:32 -0600

    NOT FINAL initial documentation

--- terranAgent.py
+++ terranAgent.py
@@ -1,7 +1,7 @@
 """ Import necessary libraries """
 
 from pysc2.agents import base_agent
-from pysc2.env import sc2_env
+from pysc2.env import sc2_env, run_loop
 from pysc2.lib import actions, features, units
 from absl import app
 import random
@@ -391,29 +391,33 @@ class TerranAgent(base_agent.BaseAgent):
   """
 def main(unused_argv):
   agent = TerranAgent()
+  agent2 = ProtossAgent()
   try:
     while True:
       with sc2_env.SC2Env(
           map_name="Simple64",
-          players=[sc2_env.Agent(sc2_env.Race.protoss),
-                   sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.very_easy)],
+          players=[sc2_env.Agent(sc2_env.Race.terran),
+                   sc2_env.Agent(sc2_env.Race.protoss)
+                  #  sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.very_easy)
+                   ],
           agent_interface_format=features.AgentInterfaceFormat(
               feature_dimensions=features.Dimensions(screen=84, minimap=64),
               use_feature_units=True),
           step_mul=16,
           game_steps_per_episode=0,
           visualize=True) as env:
+          run_loop.run_loop([agent,agent2], env)
           
-        agent.setup(env.observation_spec(), env.action_spec())
+        # agent.setup(env.observation_spec(), env.action_spec())
         
-        timesteps = env.reset()
-        agent.reset()
+        # timesteps = env.reset()
+        # agent.reset()
         
-        while True:
-          step_actions = [agent.step(timesteps[0])]
-          if timesteps[0].last():
-            break
-          timesteps = env.step(step_actions)
+        # while True:
+        #   step_actions = [agent.step(timesteps[0])]
+        #   if timesteps[0].last():
+        #     break
+        #   timesteps = env.step(step_actions)
       
   # Interrupt the code from running using ctrl + C on the terminal the file is being run at.
   except KeyboardInterrupt:
